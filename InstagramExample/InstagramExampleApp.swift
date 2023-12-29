14de44bca9f68c917b7d12fa5d89d3902d95fa53
Bolivar <bolhy91@gmail.com>
2023-12-29 10:40:09 -0500

    Initial Commit

---
+++ InstagramExample/InstagramExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramExampleApp.swift
+//  InstagramExample
+//
+//  Created by Bolivar Cortes on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
