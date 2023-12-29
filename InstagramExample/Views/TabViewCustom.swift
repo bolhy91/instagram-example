a61266ca1037409535957d8d1b6b7ce1a6fc76b1
Bolivar <bolhy91@gmail.com>
2023-12-29 16:54:48 -0500

    Creacion del tabview y los posts de los users

---
+++ InstagramExample/Views/TabViewCustom.swift
@@ -0,0 +1,18 @@
+//
+//  TabViewCustom.swift
+//  InstagramExample
+//
+//  Created by Bolivar Cortes on 12/29/23.
+//
+
+import SwiftUI
+
+struct TabViewCustom: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabViewCustom()
+}
---
+++ InstagramExample/Views/TimelineView.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineView.swift
+//  InstagramExample
+//
+//  Created by Bolivar Cortes on 12/29/23.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimelineView()
+}
